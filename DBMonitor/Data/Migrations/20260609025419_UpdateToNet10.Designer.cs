e339c132557ea31742e31410fedc8321279fa0c8
gar-mart <garmartirosy@gmail.com>
2026-06-08 22:56:40 -0400

    changed .net version to 10

--- DBMonitor/Data/Migrations/20260609025419_UpdateToNet10.Designer.cs
+++ DBMonitor/Data/Migrations/20260609025419_UpdateToNet10.Designer.cs
@@ -4,6 +4,7 @@ using DBMonitor.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DBMonitor.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260609025419_UpdateToNet10")]
+    partial class UpdateToNet10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
